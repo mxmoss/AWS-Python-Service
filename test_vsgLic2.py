e8bc3750f11668e42f0d49e30d3daba3f68cbb94
Moss Drake <mxmossman@gmail.com>
2022-09-12 12:04:10 -0700

    Update test_vsgLic2.py
    Implement tests:
    - load file
    - get customer
    - validate

--- test_vsgLic2.py
+++ test_vsgLic2.py
@@ -1,27 +1,49 @@
 from unittest import TestCase
-
+from vsgLic2 import VsgLicense
 
 class TestVsgLicense(TestCase):
+    def test_load_file(self):
+        try:
+            v = VsgLicense()
+            customers = v.load_file()
+            v.save_file(customers)
+        except:
+            self.fail()
+
     def test_get_customer(self):
-        self.fail()
+        try:
+            v = VsgLicense()
+            customers = v.load_file()
+            v.save_file(customers)
+        except:
+            self.fail()
 
     def test_validate(self):
-        self.fail()
-
-    def test_load_file(self):
-        self.fail()
-
-    def test_save_file(self):
-        self.fail()
+        try:
+            v = VsgLicense()
+            customers = v.load_file()
+            customer = v.get_customer("1234")
+            #Should validate correctly
+            if not v.validate(customer, "5544"):
+                self.fail()
+            #Should not validate correctly
+            if v.validate(customer, "1234"):
+                self.fail()
+            #Should not validate correctly
+            if v.validate(customer, ""):
+                self.fail()
+        except:
+            self.fail()
 
     def test_add(self):
+        v = VsgLicense()
         self.fail()
 
     def test_update(self):
+        v = VsgLicense()
         self.fail()
 
     def test_delete(self):
+        v = VsgLicense()
         self.fail()
 
-    def test_reset(self):
-        self.fail()
