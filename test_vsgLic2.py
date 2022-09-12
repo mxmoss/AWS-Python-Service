43a3268a49a97efc29888a6a89e60181bf03cdde
Moss Drake <mxmossman@gmail.com>
2022-09-12 11:45:42 -0700

    Create test_vsgLic2.py
    stub out unittests

---
+++ test_vsgLic2.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+
+
+class TestVsgLicense(TestCase):
+    def test_get_customer(self):
+        self.fail()
+
+    def test_validate(self):
+        self.fail()
+
+    def test_load_file(self):
+        self.fail()
+
+    def test_save_file(self):
+        self.fail()
+
+    def test_add(self):
+        self.fail()
+
+    def test_update(self):
+        self.fail()
+
+    def test_delete(self):
+        self.fail()
+
+    def test_reset(self):
+        self.fail()
